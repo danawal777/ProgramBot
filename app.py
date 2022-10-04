3d7505db3c332ac4ee6749056c4e385f5a033be1
themishasfrt <thecreatorez@gmail.com>
2022-10-04 14:11:48 +0300

    2

---
+++ app.py
@@ -0,0 +1,8 @@
+import data.functions
+import data.numbers
+
+
+sum = data.functions.sum_numbers(n1 = data.numbers.a,
+                           n2 = data.numbers.b,
+                           n3 = data.numbers.c)
+print(sum)
---
+++ data/functions.py
@@ -0,0 +1,6 @@
+from . import numbers
+
+
+def sum_numbers(*,n1,n2,n3):
+    sum = n1 + n2 + n3
+    return sum
